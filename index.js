8b567931fe764ebf8521aa1afbdca9c71f383690
Shawn Webster <swolfgangwebster@gmail.com>
2023-03-03 19:37:12 -0600

    I want to play civ5

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+function writeCards(a,e) {
+    let r = [];
+    for (let i=0;i<a.length;i++){
+        r[i] = `Thank you, ${a[i]}, for the wonderful ${e} gift!`
+    }
+
+    return r
+}
+
+//console.log(writeCards(["Shawn","Whore", "mike"],"sandy"))
+
+function countDown(n) {
+    while(n >= 0){
+        console.log(n);
+        n--;
+    }
+}
